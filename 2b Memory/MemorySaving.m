4025c028d79a9c579060dea12c0635b4b40e5689
Annija Balode <annijabalode2001@gmail.com>
2021-01-28 18:04:44 +0000

    Created LoadAllData.m file.
    Created LoadAllData.m file which involves the function for loading all of the data and the memory used by the variables is recorded.

--- 2b Memory/MemorySaving.m
+++ 2b Memory/MemorySaving.m
@@ -6,15 +6,7 @@ FileName = '5011CEM\Model\o3_surface_20180701000000.nc'; % define the name of th
 
 Contents = ncinfo(FileName); % Store the file content information in a variable.
 
-
-%% Section 2: Load all the model data together
-for idx = 1: 8
-    AllData(idx,:,:,:) = ncread(FileName, Contents.Variables(idx).Name);
-    fprintf('Loading %s\n', Contents.Variables(idx).Name); % display loading information
-end
-
-AllDataMem = whos('AllData').bytes/1000000;
-fprintf('Memory used for all data: %.3f MB\n', AllDataMem)
+LoadAllData;
 
 %% Section 3: Loading all the data for a single hour from all the models
 % We combine the aboce code to cycle through the names and load each model.
