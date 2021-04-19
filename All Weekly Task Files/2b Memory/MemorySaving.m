696a46a6d14e4d658afdecf6cbfe0ba8a6564944
Annija Balode <annijabalode2001@gmail.com>
2021-04-19 10:34:05 +0100

    Re-organisation of files and folders.
    Placed all weekly tasks under one file to tidy up the project. Put sequential and parallel processing files under one folder  and put the testing file under the test folder. This was done to tidy the project and make it more organised.

---
+++ All Weekly Task Files/2b Memory/MemorySaving.m
@@ -0,0 +1,15 @@
+%% This script allows you to open and explore the data in a *.nc file
+clear all % clear all variables
+close all % close all windows
+
+FileName = '5011CEM2021balodea2\Model\o3_surface_20180701000000.nc'; % define the name of the file to be used, the path is included
+
+Contents = ncinfo(FileName); % Store the file content information in a variable.
+
+[AllDataMem] = LoadAllData(FileName);
+
+[HourDataMem] = LoadHours(FileName);
+
+[HourMem] = LoadAllHours(FileName);
+
+ReportResults;
